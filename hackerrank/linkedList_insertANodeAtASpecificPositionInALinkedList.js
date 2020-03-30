1e8cdec4e1eaebc0de21dddbc7c54c5d63d7951c
Linh Pham <khfan_bn@yahoo.com>
2020-03-30 10:05:24 +0800

    update questions

--- hackerrank/linkedList_insertANodeAtASpecificPositionInALinkedList.js
+++ hackerrank/linkedList_insertANodeAtASpecificPositionInALinkedList.js
@@ -2,7 +2,7 @@
 
 // A position of 0 indicates head, a position of 1 indicates one node away from the head and so on.The head pointer given may be null meaning that the initial list is empty.
 
-// As an example, if your list starts as and you want to insert a node at position  with , your new list should be
+// As an example, if your list starts as 1->2->3 and you want to insert a node at position 2 with data=4, your new list should be 1->2->4->3
 
 // Function Description Complete the function insertNodeAtPosition in the editor below.It must return a reference to the head node of your finished list.
 
@@ -11,12 +11,17 @@
 // head: a SinglyLinkedListNode pointer to the head of the list
 // data: an integer value to insert as data in your new node
 // position: an integer position to insert the new node, zero based indexing
-// Input Format
+// Input Format:
 
-// The first line contains an integer, the number of elements in the linked list.
-// Each of the next  lines contains an integer SinglyLinkedListNode[i].data.
-// The next line contains an integer  denoting the data of the node that is to be inserted.
-// The last line contains an integer.
+// The first line contains an integer n, the number of elements in the linked list.
+// Each of the next n lines contains an integer SinglyLinkedListNode[i].data.
+// The next line contains an integer data denoting the data of the node that is to be inserted.
+// The last line contains an integer position.
+
+// Constraints
+//   * 1 <= n <= 1000
+//   * 1 <= SinglyLinkedListNode[i].data <= 1000
+//   * 0 <= position <= n
 
 const { SinglyLinkedList, SinglyLinkedListNode, printSinglyLinkedList } = require('../basic/singly_linked_list');
 
